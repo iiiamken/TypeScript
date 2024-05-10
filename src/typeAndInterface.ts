59c6aea9aa7cd04343932a6baefb7c15767fcb9b
iiiamken <kenny.t.dinh93@gmail.com>
2024-05-10 22:08:27 +0200

    TS fucntions

--- src/typeAndInterface.ts
+++ src/typeAndInterface.ts
@@ -13,3 +13,12 @@ let myName: "Dave"
 
 let username: "Dave" | "John" | "Mike"
 username = "John"
+
+//functions
+const add = (a: number, b: number): number => {
+  return a + b
+}
+
+const logMsg = (message: any): void => {
+  console.log(message)
+}
