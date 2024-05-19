c73c01f5946e58c9e3d16b6ccf45581b4044e223
iiiamken <kenny.t.dinh93@gmail.com>
2024-05-19 23:40:41 +0200

    added TS classes exmaple with modifiers

---
+++ src/classes.ts
@@ -0,0 +1,11 @@
+class Coder {
+  music: string
+  age: number
+  lang: string
+  constructor(public name: string, music: string, age: number, lang: string) {
+    this.name = name
+    this.music = music
+    this.age = age
+    this.lang = lang
+  }
+}
