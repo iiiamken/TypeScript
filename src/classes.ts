417054d25dc2ac5ab9b94da47789787f12a208df
iiiamken <kenny.t.dinh93@gmail.com>
2024-05-19 23:50:02 +0200

    public vs private exmaples

--- src/classes.ts
+++ src/classes.ts
@@ -1,11 +1,23 @@
 class Coder {
-  music: string
-  age: number
   lang: string
-  constructor(public name: string, music: string, age: number, lang: string) {
+  constructor(
+    public name: string,
+    private music: string,
+    protected age: number,
+    lang: string = "TypeScript"
+  ) {
     this.name = name
     this.music = music
     this.age = age
     this.lang = lang
   }
+  public getAge() {
+    return `Hello, im ${this.age}` //will work bcus age is protected
+  }
 }
+
+const Dave = new Coder("Dave", "rock", 42, "typescript")
+//private vs public modifier
+console.log(Dave.getAge()) //public method
+console.log(Dave.age) //public property
+console.log(Dave.music) //private property
