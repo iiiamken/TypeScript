064aec419849ec667f57423d7dcb3e0bf931e6e6
iiiamken <kenny.t.dinh93@gmail.com>
2024-05-20 18:49:16 +0200

    added an index signature and ex of dynamic accessing

--- src/indexSignatures.ts
+++ src/indexSignatures.ts
@@ -1,11 +1,30 @@
 interface TransactionObj {
-  Pizza: number
-  Books: number
-  Job: number
+  [key: string]: number //index signature
 }
+
+// interface TransactionObj {
+//   Pizza: number
+//   Books: number
+//   Job: number
+// }
 
 const todaysTransactions: TransactionObj = {
   Pizza: -10,
   Books: -5,
   Job: 50,
+}
+
+console.log(todaysTransactions.Pizza)
+console.log(todaysTransactions["Pizza"])
+
+//dynamic selecting ex.
+let prop: string = "Pizza"
+console.log(todaysTransactions[prop])
+
+const todaysNet = (transactions: TransactionObj) => {
+  let total = 0
+  for (const transaction in transactions) {
+    total += transactions[transaction]
+  }
+  return total
 }
