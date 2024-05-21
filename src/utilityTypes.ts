56c1be80f55e40566ec8d8f1278c51edf66fde5d
iiiamken <kenny.t.dinh93@gmail.com>
2024-05-21 20:01:53 +0200

    added utility types Record, Readonly, Required, Partial

--- src/utilityTypes.ts
+++ src/utilityTypes.ts
@@ -21,3 +21,30 @@ const assign1: Assignment = {
 }
 
 const assignGraded: Assignment = updateAssignment(assign1, { grade: 95 })
+
+//Required
+
+const recordAssignment = (assign: Required<Assignment>) => {
+  //send to db
+  return assign
+}
+//Readonly
+const assignVerified: Readonly<Assignment> = {
+  ...assignGraded,
+  verified: true,
+}
+
+//Record
+const hexColorMap: Record<string, string> = {
+  red: "#ff0000",
+  green: "#00ff00",
+  blue: "#0000ff",
+}
+
+type Students = "Sara" | "Kelly"
+type LetterGrades = "A" | "B" | "C" | "D" | "F"
+
+const finalGrades: Record<Students, LetterGrades> = {
+  Sara: "B",
+  Kelly: "A",
+}
